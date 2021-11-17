c934e727a2f794c7b1118c9a29c35354cc22d0c9
SadeByScar <sander.goertz@gmx.de>
2021-11-17 12:23:05 +0100

    SUD

--- mitarbeiter/Mitarbeiter.java
+++ mitarbeiter/Mitarbeiter.java
@@ -12,43 +12,52 @@ package mitarbeiter
  */
 public class Mitarbeiter {
 
-	private int id;
-	private String name;
-	
-	/**
-	 * 
-	 * @param id 
-	 * 
-	 */
-	public void setId(int id)
-	{
-		this.id = id;
-	}
-	
-	public int getId()
-	{
-		return this.id;
-	}
-	
-	/**
-	 * 
-	 * @param name 
-	 * 
-	 */
-	public void setName(String name)
-	{
-		this.name = name;
-	}
-	
-	public String getName()
-	{
-		return this.name;
-	}
-	
-	public String toString()
-	{
-		String rueckgabe = "Der Mitarbeiter mit der ID " + this.id + " heißt " + this.name;
-		return rueckgabe;
-	}
+  private int id;
+  private String name;
+  
+  Mitarbeiter(int id, String name)
+  {
+    setId(id);
+    setName(name);
+  }
+  
+  /**
+   * 
+   * @param id 
+   * 
+   */
+  public void setId(int id)
+  {
+    if(id > 999 && id <10000 )
+    {
+      this.id = id;
+    }
+  }
+  
+  public int getId()
+  {
+    return this.id;
+  }
+  
+  /**
+   * 
+   * @param name 
+   * 
+   */
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+  
+  public String getName()
+  {
+    return this.name;
+  }
+  
+  public String toString()
+  {
+    String rueckgabe = "Der Mitarbeiter mit der ID " + this.id + " heißt " + this.name;
+    return rueckgabe;
+  }
 
 }
