47c45fe70132683e165c028af860ead644deb86b
SadeByScar <sander.goertz@gmx.de>
2022-08-25 13:10:08 +0200

    first part of gui

---
+++ mitarbeiter/Verwaltung/VerwaltungsGUI.java
@@ -0,0 +1,15 @@
+package Verwaltung;
+
+import javax.swing.*;
+import Verwaltung.Unternehmensverwaltung;
+
+public class VerwaltungsGUI {
+    JLabel companyName;
+    JComboBox<JButton> menu;
+    JButton neu;
+    JButton delete;
+    JTextField search;
+    JMenuBar side_menu;
+    JFrame frame;
+    JPanel panel;
+}
